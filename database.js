de9c210b6ba0ca1a7b2549317d91cb4a18bba470
RogerUrrutia <rogeralexei11@gmail.com>
2022-02-15 21:50:20 -0500

    Heroku Config

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const config = {
-    user: "postgres",
-    host: "localhost",
-    database: "BlockBuster",
-    password: "laura1311",
+    user: "adrunhkuuykxck",
+    host: "ec2-23-20-73-25.compute-1.amazonaws.com",
+    database: "d2teslu31fbrk6",
+    password: "958ae7d95eb2e13f5fdf576edbf4c1b0d553adb29c9cd6a478fbe3968858815a",
     port: "5432"
 }
 
--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use("/", moviesRoutes);
 sequelize.sync()
 
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT ||3000, () =>{
     console.log("Listening on port 3000")
 })
